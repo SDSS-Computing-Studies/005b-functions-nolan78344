10cf56363261957dc4eb9782392f5db41e802dbd
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-03-02 09:51:26 -0800

    adwadw

--- problem2.py
+++ problem2.py
@@ -9,7 +9,12 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 
 import math
-#def distance([x,y] , [x1,y1]):
-#    c = tuple(((x1 - x)**2 + (y1 - y)**2))
-#    c = math.sqrt(c)
-#   return c
+def distance(a,b):
+    x = a[0]
+    y = a[1]
+    x1 = b[0]
+    y2 = b[1]
+    c = ((x1 - x)**2 + (y1 - y)**2)
+    c = math.sqrt(c)
+    return c
+
